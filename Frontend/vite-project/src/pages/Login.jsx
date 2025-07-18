1bdeb9d42f6dd13724aad0806c00afddf66df199
caspr117 <caspr117@gmail.com>
2025-07-18 13:30:12 -0400

    form submit user

--- Frontend/vite-project/src/pages/Login.jsx
+++ Frontend/vite-project/src/pages/Login.jsx
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import api from "../api";
 
 
 const[isUser, setisUser] = useState(false)
+const[form, setForm] = useState({firstName:'', lastName: '', pin: ''})
+
+const formChange = (e)=>{
+    setForm({...form, [e.target.name]: e.target.value})
+}
+
+const userSubmit = async (e) => {
+    e.preventDefault()
+}
