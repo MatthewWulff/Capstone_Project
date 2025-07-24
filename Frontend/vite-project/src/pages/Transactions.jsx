f0bc9c1412759c57feff64f864d443a7077e7ecb
caspr117 <caspr117@gmail.com>
2025-07-24 15:06:14 -0400

    back button in transaction.jsx

--- Frontend/vite-project/src/pages/Transactions.jsx
+++ Frontend/vite-project/src/pages/Transactions.jsx
@@ -1,10 +1,16 @@
 import {useParams} from "react-router-dom"
 import{useEffect, useState} from "react"
 import api from "../api"
+import { useNavigate } from "react-router-dom"
 
 export default function Transactions(){
+    const navigate =useNavigate()
     const {id} = useParams()
     const[transactions, setTransactions] = useState([])
+
+    const backToDashboard = () => {
+        navigate(-1)
+    }
  
 
 
@@ -20,6 +26,7 @@ return(
     <>
         <h2>Transactions</h2>
         {transactions.length === 0 ? <p>No Transactions recorded</p> : <ul>{transactions.map(transaction => (<li key={transaction._id}>{transaction.type.toUpperCase()} ${transaction.amount} on  {new Date(transaction.date).toLocaleString()} </li>))}</ul>}
+        <button onClick ={backToDashboard}>Back</button>
 </>
 )
 
